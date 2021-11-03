f95ac33130a3c8e58cbec8c3e95f69dda5e7b92a
Никита Раненко <nranenko@bk.ru>
2021-11-03 10:04:55 +0300

    Открытие файла и чтение содержимого

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,14 @@
-﻿using QuipuTestTask.ViewModels.Base;
+﻿using Microsoft.Win32;
+using QuipuTestTask.Commands;
+using QuipuTestTask.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace QuipuTestTask.ViewModels
 {
@@ -12,14 +17,44 @@ namespace QuipuTestTask.ViewModels
 		#region Constructor
 		public MainWindowViewModel()
 		{
-
+			this.BrowseFileCommand = new RelayCommand(OnBrowseFileCommandExecuted, CanBrowseFileCommandExecute);
 		}
 		#endregion
 
 		#region Properties
+		private string _FilePath;
+		public string FilePath
+		{
+			get => _FilePath;
+			set => Set(ref _FilePath, value);
+		}
+
+		private string _FileContent;
+		public string FileContent
+		{
+			get => _FileContent;
+			set
+			{
+				Set(ref _FileContent, value);
+				MessageBox.Show($"Содержимое файла {this.FilePath}:\n{this.FileContent}");
+			}
+		}
 		#endregion
 
 		#region Commands
+		public ICommand BrowseFileCommand { get; }
+		private bool CanBrowseFileCommandExecute(object p) => true;
+		public void OnBrowseFileCommandExecuted(object p)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Text files (*.txt)|*.txt";
+
+			if (openFileDialog.ShowDialog() == true)
+			{
+				this.FilePath = openFileDialog.FileName;
+				this.FileContent = File.ReadAllText(this.FilePath);
+			}
+		}
 		#endregion
 
 		#region Methods
