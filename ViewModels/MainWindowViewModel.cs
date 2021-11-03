9133e088dfcee3f6a02db544d9834a24f7bff36f
Никита Раненко <nranenko@bk.ru>
2021-11-03 09:52:16 +0300

    Создан каркас MVVM

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using QuipuTestTask.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuipuTestTask.ViewModels
+{
+	public class MainWindowViewModel : BaseViewModel
+	{
+		#region Constructor
+		public MainWindowViewModel()
+		{
+
+		}
+		#endregion
+
+		#region Properties
+		#endregion
+
+		#region Commands
+		#endregion
+
+		#region Methods
+		#endregion
+	}
+}
